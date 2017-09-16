049ee45da32fe15d9c9a3bac9a79b443737ea4ec
asfaca <asfaca@naver.com>
2017-09-16 22:55:01 +0900

    modify method to calculate avg green ratio in pixels

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@ using namespace cv;
 using namespace std;
 int main(int argc, char** argv)
 {
-	String imageName = "leaf2.jpg";
+	if (argc != 2) {
+		return 1;
+	}
+	String imageName = argv[1];
 	double greenRatio = 0;
 	double rgbSum = 0;
 	int count = 0;
@@ -25,14 +28,14 @@ int main(int argc, char** argv)
 	
 	for (int i = 0; i < image.rows; i++) {
 		for (int k = 0; k < image.cols * 3; k+=3) {
-			if (image.at<uchar>(i, k + 1) > image.at<uchar>(i, k) && image.at<uchar>(i, k + 1) > image.at<uchar>(i, k + 2)) {
+			if (image.at<uchar>(i, k + 1) > image.at<uchar>(i, k) + 50 && image.at<uchar>(i, k + 1) > image.at<uchar>(i, k + 2) + 50 && image.at<uchar>(i, k + 1) > 100) {
 				rgbSum += (double)image.at<uchar>(i, k + 1) / (double)(image.at<uchar>(i, k) + image.at<uchar>(i, k + 1) + image.at<uchar>(i, k + 2));
 				count++;
 			}
 		}
 	}
 
-	greenRatio = rgbSum / (double)count;
+	greenRatio = (rgbSum / (double)count) - (rgbSum / ((double)count) * 0.08); //0.08 is for error.
 
 	cout << greenRatio;
 	
